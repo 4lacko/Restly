709772ed64b0183f4575c0b6eeff8b0db03f7e10
Flacko <f@ak68a.co>
2023-05-18 22:19:03 -0400

    Initial Commit

---
+++ Restly/RestlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestlyApp.swift
+//  Restly
+//
+//  Created by Ayo Moreira on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct RestlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
